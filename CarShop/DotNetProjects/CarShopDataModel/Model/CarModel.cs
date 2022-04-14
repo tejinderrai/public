aabfffa33e91c682905404447873b9b175b29d9a
tejinderrai <tejinder.rai@hotmail.com>
2022-04-14 22:42:42 +0100

    Uploaded the CarShop Model Classes Project

---
+++ CarShop/DotNetProjects/CarShopDataModel/Model/CarModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarShopDataModel
+{
+    public class CarModel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int64 CarModelID { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Model { get; set; }
+
+        [Required][ForeignKey("ManufacturerId")]
+        public Int64 ManufacturerID { get; set; }
+    }
+}
---
+++ CarShop/DotNetProjects/CarShopDataModel/Model/VehicleStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShopDataModel
+{
+    public class VehicleStatus
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StatuseId { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string STATUS { get; set; }
+    }
+}
