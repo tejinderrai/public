e194648b4b3cb3d349223e402c53d6886b7f50e7
tejinderrai <tejinder.rai@hotmail.com>
2024-07-25 12:21:58 +0100

    Initial upload of the KeyVault Secret Reader Console App using ManagedIdentity Authentication via RBAC

---
+++ AzKeyVaultSecretReaderConsoleApp/AzKeyVaultSecretReaderConsoleApp/KeyVaultHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace AzKeyVaultSecretReaderConsoleApp
+{
+    internal class KeyVaultHelper
+    {
+        public static async Task<KeyVaultSecret> GetKeyVaultSecret(Uri KeyVaultURI, string KeyVaultSecretName)
+        {
+            var credential = new DefaultAzureCredential();            
+            var keyVaultSecretClient = new SecretClient(KeyVaultURI, new DefaultAzureCredential());
+
+            KeyVaultSecret keyVaultSecret = await keyVaultSecretClient.GetSecretAsync(KeyVaultSecretName);
+            return keyVaultSecret;
+        }
+    }
+}
---
+++ AzKeyVaultSecretReaderConsoleApp/AzKeyVaultSecretReaderConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using AzKeyVaultSecretReaderConsoleApp;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+
+IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+IConfigurationRoot appConfig = builder.Build();
+
+string kVaultURI = appConfig["KeyVaultURI"];
+string kVSecret = appConfig["KeyVaultSecretName"];
+
+try
+{
+    KeyVaultSecret secret = await KeyVaultHelper.GetKeyVaultSecret(new Uri(kVaultURI), kVSecret);
+    if (!string.IsNullOrEmpty(secret.Value))
+    {
+        Console.WriteLine("Key Vault Secret is: {0}", secret.Value);
+    }
+    else
+    {
+        Console.WriteLine("Key value is empty!");
+    }
+}
+catch (Exception ex)
+{
+    Exception e = ex;
+    Console.WriteLine($"An error occurred: {e.Message}");
+}
