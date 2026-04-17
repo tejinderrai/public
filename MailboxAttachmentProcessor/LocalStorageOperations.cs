e0a76f74d165f74747ae4b61ea2a7a5966452521
tejinderrai <tejinder.rai@hotmail.com>
2026-04-17 12:48:39 +0100

    Uploaded new code

---
+++ MailboxAttachmentProcessor/LocalStorageOperations.cs
@@ -0,0 +1,46 @@
+﻿using Azure;
+using Azure.Storage.Files.Shares;
+using CourseCompletionsProcessor;
+using Microsoft.Graph.Models;
+using System;
+using System.IO;
+
+namespace MailBoxAttachmentProcessor
+{
+    internal class LocalStorageOperations
+    {
+        public static async Task<bool> DownloadAttachmentToLocalFolder(FileAttachment attachment, string Folder)
+        {
+            bool result = false;
+
+            try
+            {
+                var item = (FileAttachment)attachment;
+
+                string filePath = Path.Combine(Folder, item.Name);
+
+                // Load and save the attachment
+                using (MemoryStream stream = new MemoryStream(item.ContentBytes))
+                {
+                    stream.Position = 0; // Reset stream position
+                    using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                    {
+                        stream.CopyTo(fs);
+                    }
+
+                    Console.WriteLine($"Saved: {filePath}");
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Exception: {ex.Message}");
+                return false;
+
+            }
+
+            return true;
+        }
+    }
+}
---
+++ MailboxAttachmentProcessor/MSGraphOperations2.cs
@@ -0,0 +1,158 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Storage.Files.Shares;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Users.Item.Messages.Item.Move;
+using System.Net.Mail;
+
+namespace CourseCompletionsProcessor
+{
+    public  class MSGraphOperations
+    {
+        public static GraphServiceClient CreateGraphClientWithApp()
+        {
+            ClientSecretCredential? _clientSecretCredential;
+            _clientSecretCredential = new ClientSecretCredential(AuthContext.TenantId,
+            AuthContext.ClientId, AuthContext.Secret, new TokenCredentialOptions { AuthorityHost = AzureAuthorityHosts.AzurePublicCloud });
+
+            var _graphClient = new GraphServiceClient(_clientSecretCredential,
+            new[] { "https://graph.microsoft.com/.default" });
+            return _graphClient;
+        }
+
+        public static GraphServiceClient CreateGraphClientWithManagedIdentity()
+        {
+            ChainedTokenCredential _manageIdentityCredential  = new ChainedTokenCredential(
+            new ManagedIdentityCredential(),
+            new EnvironmentCredential());
+
+            var _graphClient = new GraphServiceClient(_manageIdentityCredential,
+            new[] { "https://graph.microsoft.com/.default" });
+            return _graphClient;
+        }
+
+        public static async Task<AttachmentCollectionResponse> GetMessageAttachmentCollectionWithApp(Message message)
+        {
+            AttachmentCollectionResponse attachmentCollectionResponse = new AttachmentCollectionResponse();
+
+            try
+            {
+                var _graphClient = MSGraphOperations.CreateGraphClientWithApp();
+                attachmentCollectionResponse = await _graphClient.Users[References.MailEmailAddress].Messages[message.Id].Attachments.GetAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return attachmentCollectionResponse;
+        }
+
+        public static async Task<AttachmentCollectionResponse> GetMessageAttachmentCollectionWithManagedIdentity(Message message)
+        {
+            AttachmentCollectionResponse attachmentCollectionResponse = new AttachmentCollectionResponse();
+
+            try
+            {
+                var _graphClient = MSGraphOperations.CreateGraphClientWithManagedIdentity();
+                attachmentCollectionResponse = await _graphClient.Users[References.MailEmailAddress].Messages[message.Id].Attachments.GetAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return attachmentCollectionResponse;
+        }
+
+        public static async Task<MessageCollectionResponse> GetMessageCollectionWithApp()
+        {
+            MessageCollectionResponse messageCollectionResponse = new MessageCollectionResponse();
+
+            try
+            {
+                var _graphClient = MSGraphOperations.CreateGraphClientWithApp();
+                messageCollectionResponse = await _graphClient.Users[References.MailEmailAddress].MailFolders[References.MailFolderName].Messages.GetAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return messageCollectionResponse;
+        }
+
+        public static async Task<MessageCollectionResponse> GetMessageCollectionWithManagedIdentity()
+        {
+            MessageCollectionResponse messageCollectionResponse = new MessageCollectionResponse();
+
+            try
+            {
+                var _graphClient = MSGraphOperations.CreateGraphClientWithManagedIdentity();
+                messageCollectionResponse = await _graphClient.Users[References.MailEmailAddress].MailFolders[References.MailFolderName].Messages.GetAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return messageCollectionResponse;
+        }
+
+
+        public static async Task<bool> MoveProcessedMessagesToProcessedFolder(string MessageId)
+        {
+            var _graphClient = MSGraphOperations.CreateGraphClientWithManagedIdentity();
+            
+            // Iterate the mailbox to get the folder ID equal to ProcessedMessages i.e. do not hard code folder id it is computed at runtime
+            var folders = _graphClient.Users[References.MailEmailAddress].MailFolders.GetAsync().Result;
+            var folderId = "";
+
+            foreach (var folder in folders.Value)
+            {
+                if (folder.DisplayName == References.ProcessedMessagesFolderName)
+                {
+                    folderId = folder.Id;
+                }
+            }
+
+            // Create the request body for the move operation - the destination folder Id
+            var requestBody = new MovePostRequestBody
+            {
+                DestinationId = folderId,
+            };
+
+            // Move the message to processed messages folder
+            var moved = _graphClient.Users[References.MailEmailAddress].Messages[MessageId].Move.PostAsync(requestBody).Result;
+            return true;
+        }
+
+        public static async Task<bool> MoveProcessedMessagesToProcessedFolderWithMangedIdentity(string MessageId)
+        {
+            var _graphClient = MSGraphOperations.CreateGraphClientWithManagedIdentity();
+
+            // Iterate the mailbox to get the folder ID equal to ProcessedMessages i.e. do not hard code folder id it is computed at runtime
+            var folders = _graphClient.Users[References.MailEmailAddress].MailFolders.GetAsync().Result;
+            var folderId = "";
+
+            foreach (var folder in folders.Value)
+            {
+                if (folder.DisplayName == References.ProcessedMessagesFolderName)
+                {
+                    folderId = folder.Id;
+                }
+            }
+
+            // Create the request body for the move operation - the destination folder Id
+            var requestBody = new MovePostRequestBody
+            {
+                DestinationId = folderId,
+            };
+
+            // Move the message to processed messages folder
+            var moved = _graphClient.Users[References.MailEmailAddress].Messages[MessageId].Move.PostAsync(requestBody).Result;
+            return true;
+        }
+    }
+}
